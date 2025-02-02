afa7f04e2c8c9149841dffe2910062dd57577bc9
Gabriel Schneider <gabrielmunizsch@gmail.com>
2025-02-02 00:37:08 -0300

    feat: criados os arquivos de modelos para retornos do crawler

---
+++ Crawler.Core/Models/CrawlerResult.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMDB_Crawler.Crawler.Core.Models
+{
+    public interface ICrawlerResult
+    {
+        string Name { get; set; }
+        int ReleaseYear { get; set; }
+        string Director { get; set; }
+        double AverageRating { get; set; }
+        int NumberOfRatings { get; set; }
+    }
+
+    // Criação de uma classe concreta que implementa a interface ICrawlerResult
+    public class CrawlerResult : ICrawlerResult
+    {
+        public string Name { get; set; }
+        public int ReleaseYear { get; set; }
+        public string Director { get; set; }
+        public double AverageRating { get; set; }
+        public int NumberOfRatings { get; set; }
+    }
+}
---
+++ Crawler.Core/Models/MoveInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crawler_Data_Lawer.Crawler.Core.Models
+{
+    public class MovieInfo
+    {
+        public int ReleaseYear { get; set; }
+        public string Director { get; set; }
+    }
+
+}
